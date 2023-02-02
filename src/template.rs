be340523ec9ff2ae1985deeb4ccfcb2a6f1a431f
William Roberts <william.roberts@stridehealth.com>
2023-02-02 16:32:39 -0700

    use add_template_file

--- src/template.rs
+++ src/template.rs
@@ -3,9 +3,13 @@ use std::{error::Error, fs, io::Write, path::Path};
 use tera::{Context, Tera};
 
 pub fn get(template_name: &str, name: &str) -> Result<String, Box<dyn Error>> {
-    let tera = Tera::new("src/templates/**/*.txt").unwrap();
+    let mut tera = Tera::new("src/templates/**/*.txt").unwrap();
+    tera.add_template_file("src/templates/component.txt", None)?;
+    tera.add_template_file("src/templates/component-test.txt", None)?;
+
     let mut context = Context::new();
     context.insert("name", &name);
+
     let template = tera.render(template_name, &context)?;
 
     Ok(template)
